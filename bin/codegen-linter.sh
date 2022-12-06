064b7b0ae189b9f763d00b2dbe7465d8cd2cdcd1
Jason Mandel <jmandel@squareup.com>
2022-12-06 16:38:31 -0500

    goddamnit lol

--- bin/codegen-linter.sh
+++ bin/codegen-linter.sh
@@ -22,21 +22,24 @@ function buf_lint() {
 }
 
 function lint_codegen() {
+  #git_hash=$(git rev-parse --short "$GITHUB_SHA")
+  git_branch=${GITHUB_REF#refs/heads/}
+
   echo "about to lint codegen"
-  echo $(git diff --quiet main -- services/migration/src/perspex || echo $?)
-  if [[ "$(git diff --quiet refs/remotes/origin/HEAD -- services/migration/src/perspex || echo $?)" == "1" ]]; then
+  echo $(git diff --quiet main..${git_branch} -- services/migration/src/perspex || echo $?)
+  if [[ "$(git diff --quiet main..${git_branch} -- services/migration/src/perspex || echo $?)" == "1" ]]; then
     echo "perspex changed"
     genpath="schemas/perspex/pkg/models"
     tool="sqlboiler"
     verify_hashes
-  echo $(git diff --quiet main -- schemas/graphql || echo $?)
+  echo $(git diff --quiet main..${git_branch}  -- schemas/graphql || echo $?)
   elif [[ "$(git diff --quiet main -- schemas/graphql || echo $?)" == "1" ]]; then
     echo "graphql changed"
     genpath="schemas/graphql/pkg"
     tool="gqlgen"
     verify_hashes
-  echo $(git diff --quiet main -- schemas/proto/**/*.proto || echo $?)
-  elif [[ "$(git diff --quiet main -- schemas/proto/**/*.proto || echo $?)" == "1" ]]; then
+  echo $(git diff --quiet main..${git_branch} -- schemas/proto/**/*.proto || echo $?)
+  elif [[ "$(git diff --quiet main..${git_branch} -- schemas/proto/**/*.proto || echo $?)" == "1" ]]; then
     echo "proto changed"
     genpath="schemas/proto/goproto"
     tool="buf"
