ceaa245dc4f68977dfd53e41d74e12cfd0aa3a9b
Jason Mandel <jmandel@squareup.com>
2022-12-06 20:34:51 -0500

    adding quiet mode for git

--- bin/codegen-linter.sh
+++ bin/codegen-linter.sh
@@ -15,7 +15,7 @@ function verify_hashes() {
   fi
 
   echo "about to checkout ${genpath} main"
-  git checkout --theirs "${genpath}" --quiet main
+  git checkout -q origin/main -- "${genpath}"
   
   main=$(tar -cf - "${genpath}" | md5sum)
   echo "main: ${main}"
