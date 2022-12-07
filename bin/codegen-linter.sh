7908d97267884914dbed7d3e453c740515c2ee55
Jason Mandel <jmandel@squareup.com>
2022-12-06 20:05:55 -0500

    fix: made codegen way simpler

--- bin/codegen-linter.sh
+++ bin/codegen-linter.sh
@@ -13,46 +13,16 @@ function verify_hashes() {
     echo "Error: Generated ${tool} code is out of phase, please commit generated code."
     exit 1;
   fi
+
+  if [[ "${tool}" == "buf" ]]; then
+    buf_lint
+  fi
 }
 
 function buf_lint() {
   cd schemas/proto
   ../../bin/buf lint
   cd ../..
-}
-
-function lint_codegen() {
-  echo "about to lint codegen"
-  main_branch=$(git rev-parse --short main)
-  changes=$(git diff --name-only ${main_branch})
-  for change in ${changes}; do
-    echo "change: ${change}"
-  done
-
-  echo $(git diff --quiet ${main_branch} -- services/migration/src/perspex || echo $?)
-  if [[ "$(git diff --quiet  ${main_branch} -- services/migration/src/perspex || echo $?)" == "1" ]]; then
-    echo "perspex changed"
-    genpath="schemas/perspex/pkg/models"
-    tool="sqlboiler"
-    verify_hashes
-  echo $(git diff --quiet ${main_branch} -- schemas/graphql || echo $?)
-  elif [[ "$(git diff --quiet ${main_branch} -- schemas/graphql || echo $?)" == "1" ]]; then
-    echo "graphql changed"
-    genpath="schemas/graphql/pkg"
-    tool="gqlgen"
-    verify_hashes
-  echo $(git diff --quiet ${main_branch} -- schemas/proto/**/*.proto || echo $?)
-  elif [[ "$(git diff --quiet ${main_branch} -- schemas/proto/**/*.proto || echo $?)" == "1" ]]; then
-    echo "proto changed"
-    genpath="schemas/proto/goproto"
-    tool="buf"
-    verify_hashes
-    buf_lint
-  else
-    echo "Generated code is up to date"
-  fi
-
-  exit 0;
 }
 
 function display_help() {
@@ -69,8 +39,16 @@ function display_help() {
 main() {
   while [[ "$#" -gt 0 ]]; do 
     case $1 in
+       -g|--genpath)
+        genpath="$2"
+        shift
+        ;;
+      -t|--tool)
+        tool="$2"
+        shift
+        ;;
       -l | --lint )
-        lint_codegen
+        verify_hashes
         shift
         ;;
       -h | --help )
