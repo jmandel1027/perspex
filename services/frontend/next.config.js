293190ec9f047b87c53174ed6f1b8b6e0a0e8418
Jason Mandel <jmandel@squareup.com>
2023-01-06 09:27:09 -0500

    chre: adding safe timeouts for frontend

--- services/frontend/next.config.js
+++ services/frontend/next.config.js
@@ -1,8 +1,42 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
 }
+
+const signals = [
+  "SIGHUP",
+  "SIGINT",
+  "SIGQUIT",
+  "SIGILL",
+  "SIGTRAP",
+  "SIGABRT",
+  "SIGBUS",
+  "SIGFPE",
+  "SIGUSR1",
+  "SIGSEGV",
+  "SIGUSR2",
+  "SIGTERM",
+];
+
+const terminator = (signal) => {
+  console.log(`Received ${signal}: ", "cleaning up`);
+  setTimeout(
+    (...args) => {
+      console.log(...args);
+      process.exit(0);
+    },
+    400,
+    "Message"
+  );
+  // un-commenting this line will cause everything to just stop and kill the pending timeout
+  // process.exit(0);
+};
+
+signals.map((signal) => {
+  process.on(signal, () => terminator(signal));
+});
 
 module.exports = nextConfig
